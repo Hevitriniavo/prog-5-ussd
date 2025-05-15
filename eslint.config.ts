d13af64087dde29d50026af2c2bcbda91b8f3a87
Hevitriniavo <hei.tantely@gmail.com>
2025-05-15 21:47:55 +0300

    chore: eslint

---
+++ eslint.config.ts
@@ -0,0 +1,66 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import checkFilePlugin from "eslint-plugin-check-file";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        plugins: {
+            "check-file": checkFilePlugin,
+        },
+        ignores: [
+            "node_modules/**",
+            "dist/**",
+            "build/**",
+        ],
+
+        languageOptions: {
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            'check-file/no-index': 'error',
+            "check-file/filename-naming-convention": [
+                "error",
+                {
+                    "src/**/*.{js,ts}": "CAMEL_CASE",
+                }
+            ],
+            "check-file/folder-naming-convention": [
+                "error",
+                {
+                    "src/**/*": "SNAKE_CASE"
+                }
+            ],
+            "@typescript-eslint/naming-convention": [
+                "error",
+                {
+                    "selector": "variableLike",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "function",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "parameter",
+                    "format": ["camelCase"]
+                },
+                {
+                    "selector": "class",
+                    "format": ["PascalCase"],
+                }
+            ],
+            "@typescript-eslint/ban-ts-comment": [
+                "error",
+                { minimumDescriptionLength: 10 },
+            ],
+        },
+    }
+);
